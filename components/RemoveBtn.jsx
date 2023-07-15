ea1c46f6fd4e5b1993c2f62cd5de3ddd23fb0df4
hadiRazal <hadhirasal22@gmail.com>
2023-07-15 23:22:34 +0530

    CRUD APP COMPLETED

---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import { HiOutlineTrash } from "react-icons/hi";
+
+const RemoveBtn = ({ id }) => {
+  const router = useRouter();
+
+  const removeTopic = async () => {
+    const confirmed = confirm("are you sure?");
+    if (confirmed) {
+      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        router.refresh();
+      }
+    }
+  };
+
+  return (
+    <button onClick={removeTopic} className="text-red-500">
+      <HiOutlineTrash size={24} />
+    </button>
+  );
+};
+
+export default RemoveBtn;
